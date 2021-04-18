f0e7bf0bb7f0c35d205dde1c417ad6e59a9c6f42
codemyobj <459324704@qq.com>
2021-04-18 17:52:55 +0800

    完成了登录功能

--- Desktop/电商管理系统/code/vue_project/src/main.js
+++ Desktop/电商管理系统/code/vue_project/src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element'
+//导入全局样式表
+import './assets/css/global.css'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+//导入字体图标
+import './assets/fonts/iconfont.css'
+
+import axios from 'axios'
+axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
+//配置请求的根路径
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+Vue.use(ElementUI);
 
 new Vue({
   router,
