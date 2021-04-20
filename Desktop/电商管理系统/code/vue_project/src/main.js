d9357a32bcefe5925bd044a249abd022d18016e9
codemyobj <459324704@qq.com>
2021-04-20 16:03:40 +0800

    完成了分类功能的开发

--- Desktop/电商管理系统/code/vue_project/src/main.js
+++ Desktop/电商管理系统/code/vue_project/src/main.js
@@ -9,6 +9,10 @@ import 'element-ui/lib/theme-chalk/index.css';
 //导入字体图标
 import './assets/fonts/iconfont.css'
 
+import TreeTable from 'vue-table-with-tree-grid'
+
+
+
 import axios from 'axios'
 //配置请求的根路径
 axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
@@ -23,6 +27,8 @@ Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
+
+Vue.component('tree-table', TreeTable)
 
 new Vue({
   router,
