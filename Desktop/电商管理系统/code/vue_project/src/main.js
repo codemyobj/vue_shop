c46b3fdfae1e15b20712f6d5efda307c7046eb2d
codemyobj <459324704@qq.com>
2021-04-21 19:18:07 +0800

    完成商品功能开发

--- Desktop/电商管理系统/code/vue_project/src/main.js
+++ Desktop/电商管理系统/code/vue_project/src/main.js
@@ -11,6 +11,13 @@ import './assets/fonts/iconfont.css'
 
 import TreeTable from 'vue-table-with-tree-grid'
 
+//导入文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+//导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
 
 
 import axios from 'axios'
@@ -29,6 +36,21 @@ Vue.config.productionTip = false
 Vue.use(ElementUI);
 
 Vue.component('tree-table', TreeTable)
+//将富文本编辑器注册为全局可以用的组件
+Vue.use(VueQuillEditor)
+
+Vue.filter('dataFormat', function (originVal) {
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
